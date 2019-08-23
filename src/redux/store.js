e400505eaab6efa527233a1fcd4161cf00f74732
Raj Rathod <raaajrathod@gmail.com>
2019-08-23 13:13:16 +0530

    Bug Fixed

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,9 +4,10 @@ import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
 
-const middlewares = [];
-if (process.env.NODE_ENV === "development") {
-  middlewares[logger];
+let middlewares = [];
+
+if (process.env.NODE_ENV == "development") {
+  middlewares.push(logger);
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
